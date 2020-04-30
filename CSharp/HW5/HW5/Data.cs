5e186769c637fd2a6a68d7b0e12f2f19c4fb5041
thefairman <thefair@bk.ru>
2020-04-30 22:08:11 +0300

    hw5

---
+++ CSharp/HW5/HW5/Data.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    struct Data
+    {
+        public int A;
+        public int B;
+    }
+
+    struct LinearEquation
+    {
+        Data Data;
+        public bool TryParse(string str)
+        {
+            string[] lex = str.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (lex.Length != 2)
+            {
+                Console.WriteLine("Колличество коэфицентов != 2");
+                return false;
+            }
+            if (Int32.TryParse(lex[0], out int a))
+            {
+                Console.WriteLine("Первый коэфициент должен быть целым числом");
+                return false;
+            }
+            if (Int32.TryParse(lex[1], out int b))
+            {
+                Console.WriteLine("Второй коэфициент должен быть целым числом");
+                return false;
+            }
+            Data.A = a;
+            Data.B = b;
+            return true;
+        }
+    }
+
+}
