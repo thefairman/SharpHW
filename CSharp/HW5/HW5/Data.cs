09e81fd3a6d468c0d1419cfa43f4715be0bdc763
thefairman <meldanvik@gmail.com>
2020-05-09 11:59:38 +0300

    done hw5

--- CSharp/HW5/HW5/Data.cs
+++ CSharp/HW5/HW5/Data.cs
@@ -10,32 +10,103 @@ namespace HW5
     {
         public int A;
         public int B;
+        public int C;
+    }
+
+    struct SolvingData
+    {
+        public double X;
+        public double Y;
     }
 
     struct LinearEquation
     {
-        Data Data;
+        public Data Data { get; private set; }
         public bool TryParse(string str)
         {
             string[] lex = str.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            if (lex.Length != 2)
+            if (lex.Length != 3)
             {
-                Console.WriteLine("Колличество коэфицентов != 2");
+                Console.WriteLine("Колличество ввденных значений != 3");
                 return false;
             }
-            if (Int32.TryParse(lex[0], out int a))
+            if (!Int32.TryParse(lex[0], out int a))
             {
                 Console.WriteLine("Первый коэфициент должен быть целым числом");
                 return false;
             }
-            if (Int32.TryParse(lex[1], out int b))
+            if (!Int32.TryParse(lex[1], out int b))
             {
                 Console.WriteLine("Второй коэфициент должен быть целым числом");
                 return false;
             }
-            Data.A = a;
-            Data.B = b;
+            if (!Int32.TryParse(lex[2], out int c))
+            {
+                Console.WriteLine("Свободный член должен быть целым числом");
+                return false;
+            }
+            Data tmpData;
+            tmpData.A = a;
+            tmpData.B = b;
+            tmpData.C = c;
+            Data = tmpData;
             return true;
+        }
+
+        bool CheckFirstRankNotZero(double[,] matrix)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                bool notOk = true;
+                for (int j = 0; j < 2; j++)
+                {
+                    if (matrix[i, j] != 0)
+                    {
+                        notOk = false;
+                        break;
+                    }
+                }
+                if (notOk)
+                    return false;
+            }
+            return true;
+        }
+
+        public SolvingData SolvingEquation(LinearEquation le)
+        {
+            if (Data.A == 0 && le.Data.A == 0)
+                throw new Exception("Ни в одном из уравнений нет X");
+            double[,] matrix = { { Data.A, Data.B, -Data.C }, { le.Data.A, le.Data.B, -le.Data.C } };
+            if (!CheckFirstRankNotZero(matrix))
+                throw new Exception("Данная система не имеет решения");
+            bool swapped = false;
+            if (matrix[0, 0] == 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    matrix[0, i] = matrix[1, i];
+                }
+                matrix[1, 0] = Data.A;
+                matrix[1, 1] = Data.B;
+                matrix[1, 2] = Data.C;
+                swapped = true;
+            }
+
+            double multiplier = matrix[1, 0] / matrix[0, 0];
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[1, i] += matrix[0, i] * -multiplier;
+            }
+
+            if (!CheckFirstRankNotZero(matrix))
+                throw new Exception("Данная система не имеет решения или только одного члена");
+
+            SolvingData solvingData;
+            solvingData.Y = matrix[1, 2] / matrix[1, 1];
+            solvingData.X = (matrix[0, 2] - solvingData.Y * matrix[0, 1]) / matrix[0, 0];
+            if (swapped)
+                return new SolvingData { X = solvingData.Y, Y = solvingData.X };
+            return solvingData;
         }
     }
 
--- CSharp/HW5/HW5/Program.cs
+++ CSharp/HW5/HW5/Program.cs
@@ -6,10 +6,80 @@ using System.Threading.Tasks;
 
 namespace HW5
 {
+    class UI
+    {
+        static string PrintMiddleElem(int a)
+        {
+            return a < 0 ? $"- {-a}" : $"+ {a}";
+        }
+        static void PrintEquation(LinearEquation linearEquation)
+        {
+            Console.WriteLine($"{linearEquation.Data.A}x {PrintMiddleElem(linearEquation.Data.B)}y {PrintMiddleElem(linearEquation.Data.C)} = 0");
+        }
+        static public bool AskAgain(bool contine = true)
+        {
+            Console.WriteLine($"Чтобы {(contine ? "продолжить" : "выполнить еще раз")}, нажмите пробел");
+            return Console.ReadKey().Key == ConsoleKey.Spacebar;
+        }
+
+        static LinearEquation SetLinearEquation()
+        {
+            LinearEquation linearEquation = new LinearEquation();
+            string str;
+            do
+            {
+                Console.WriteLine("Введите данные линейного уравнения Ax + By + C = 0, ABC вводится через пробел или запятую");
+                str = Console.ReadLine();
+            } while (!linearEquation.TryParse(str));
+            return linearEquation;
+        }
+
+        static public void SolvingEquation()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите первое уравнение:");
+            LinearEquation linearEquation1 = SetLinearEquation();
+            Console.WriteLine("Введите второе уравнение:");
+            LinearEquation linearEquation2 = SetLinearEquation();
+            Console.Clear();
+            Console.WriteLine("Ваши уравнения:");
+            PrintEquation(linearEquation1);
+            PrintEquation(linearEquation2);
+            SolvingData sd;
+            try
+            {
+                sd = linearEquation1.SolvingEquation(linearEquation2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка вычисления: {ex.Message}");
+                return;
+            }
+            Console.WriteLine($"Решение системы уравнений: Х = {sd.X:0.####}; Y = {sd.Y:0.####}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            do
+            {
+                UI.SolvingEquation();
+            } while (UI.AskAgain(false));
+
+            Complex z = new Complex(1, 1);
+            Complex z1;
+            z1 = z - (z * z * z - 1) / (3 * z * z);
+            Console.WriteLine("z1 = {0}", z1);
+
+            Fraction f = new Fraction(3, 4);
+            int a = 10;
+            Fraction f1 = f * a;
+            Fraction f2 = a * f;
+            double d = 1.5;
+            Fraction f3 = f + d;
+
+            Console.WriteLine(f3);
         }
     }
 }
